f7cb1b819ae317fa9bb375b5c5515d6ce28a099e
Raúl Manzanero <74859614+RaulMLA@users.noreply.github.com>
2022-02-20 20:02:20 +0000

    mycat file (commented)

--- mycat.c
+++ mycat.c
@@ -1,64 +1,50 @@
-#include <stdio.h>		                    /* Header file for printf. */
-#include <sys/types.h>		                /* Header file for system call  open. */
+#include <stdio.h>		                    // Header file for printf. 
+#include <sys/types.h>		                // Header file for system call  open. 
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <unistd.h>		                    /* Header file for system calls read, write y close. */
+#include <unistd.h>		                    // Header file for system calls read, write y close.
 
-#define BUFFER_SIZE 1024    // 1024 bytes
+#define BUFFER_SIZE 1024                    // 1024 bytes.
 
-/*
-The first program, mycat will open the file specified by argument and will show its content
-through standard output (the console) using the calls open, read, write and close. For this
-purpose:
-• It will open the file specified as parameter with open.
-• It will read the contents of the file using an intermediate buffer of 1024 bytes (read).
-• It will write (write) the content of the buffer in the standard output. Use the constant STDOUT FILENO as value of the         descriptor to write to the standard output. 
-• Finally, it will close the descriptor using close.
-*/
 
 int main(int argc, char *argv[]) {
     
+    // Declaration of the variables the function will use.
+    // Declaration of the file descriptor.
     int descriptor;
+    // Declaration of the buffer --> Vector with length equal to the BUFFER_SIZE.                                             
     char buffer[BUFFER_SIZE];
+    // Declaration of the variable that stores the number of bytes to read.
     int nread;
   
-	/* If the number of arguments is less than two (argv[0] -> program, argv[|] -> file to be shown) we print the error and return -1. */
+	// If the number of arguments is less than two we print the error and return.
 	if(argc < 2) {
 		printf("Not enough arguments. \n");
         // Stop program.
-		return -1;
+		return(-1);
 	}
     
-    //Open the file, save its descriptor and check it to see if there's an error
+    // Open the file, save its descriptor and check it to see if there is an error. The system call 'open' is used.
     descriptor = open(argv[1], O_RDONLY);
-    if (descriptor < 0) {
-        printf("The file doesn't exist. \n");    // If descriptor is equal to 0 there's no such file.
+    // If descriptor is equal to -1 there is no such file (error), so the function will end.                       
+    if (descriptor < 0) {                                           
+        printf("The file does not exist. \n");                       
         return(-1);
     }
-    else{
-        nread = read(descriptor, buffer, BUFFER_SIZE);
-        if (nread != 0) {
-            do {
-                write(STDOUT_FILENO, buffer, nread);
-                nread = read(descriptor, buffer, BUFFER_SIZE);
-            }
-            while(nread != 0);
-        }
+    
+    // The bytes to read are stored in the declared variable. The system call 'read' is used.
+    nread = read(descriptor, buffer, BUFFER_SIZE);
+    // If the file is not empy, a loop is entered. If not, jump to the end. 
+    if (nread != 0) {
+        // Until the buffer is empty, the information of the file is written.
+        do {
+            write(STDOUT_FILENO, buffer, nread);
+            nread = read(descriptor, buffer, BUFFER_SIZE);
+        } while(nread != 0);
     }
-    close(descriptor);  
+
+    // Closes and returns 0.
+    close(descriptor);                                              
     return 0;
 }
 
-/*
-// The file cannot be opened.
-if ((infile = open(name, O_RDONLY)) < 0) {
-    return(-1);
-}
-*/
-
-/*
-The predefined descriptors are:
-    •	0 : Standard input (read from stdin).
-    •	1 : Standard output (write to stout).
-    •	2 : Error output (write to stderr).
-*/
