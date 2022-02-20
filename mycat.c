8c3d2b38846988df72906f87e138109835052e5e
Victor Valencia <75833066+victorvalchez@users.noreply.github.com>
2022-02-20 19:22:11 +0000

    mycat file

--- mycat.c
+++ mycat.c
@@ -1,35 +1,64 @@
-#include <stdio.h>		/* Header file for printf */
-#include <sys/types.h>		/* Header file for system call  open */
+#include <stdio.h>		                    /* Header file for printf. */
+#include <sys/types.h>		                /* Header file for system call  open. */
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <unistd.h>		/* Header file for system calls read, write y close */
+#include <unistd.h>		                    /* Header file for system calls read, write y close. */
 
-#define BUFFER_SIZE 1024
+#define BUFFER_SIZE 1024    // 1024 bytes
 
-int main(int argc, char *argv[])
-{
+/*
+The first program, mycat will open the file specified by argument and will show its content
+through standard output (the console) using the calls open, read, write and close. For this
+purpose:
+• It will open the file specified as parameter with open.
+• It will read the contents of the file using an intermediate buffer of 1024 bytes (read).
+• It will write (write) the content of the buffer in the standard output. Use the constant STDOUT FILENO as value of the         descriptor to write to the standard output. 
+• Finally, it will close the descriptor using close.
+*/
 
-	/*If the number of arguments is less than two (argv[0] -> program, argv[|] -> file to be shown) we print 	 the error and return -1*/
-	if(argc < 2)
-	{
-		printf("Not enough arguments\n");
-		return -1; /* Stop program*/
+int main(int argc, char *argv[]) {
+    
+    int descriptor;
+    char buffer[BUFFER_SIZE];
+    int nread;
+  
+	/* If the number of arguments is less than two (argv[0] -> program, argv[|] -> file to be shown) we print the error and return -1. */
+	if(argc < 2) {
+		printf("Not enough arguments. \n");
+        // Stop program.
+		return -1;
 	}
-	printf("%s\n", argv[1]); /* Print the file name */
-	int fichero = open(argv[1], O_RDONLY); /* open the file in read only mode*/
-	if (fichero < 1){ 	/* If the readed file showed no problems we continue*/
-		printf("El fichero selecionado no existe\n");
-		return -1; 	/* Stop program*/
-	}
-	char guardado[BUFFER_SIZE];  /*Var to save the readed file*/
-	int leido = read(fichero, guardado, BUFFER_SIZE); /*Read the file and check errors*/
-	while (leido >0){
-		write(STDOUT_FILENO, guardado, leido);  /*WHile there is smth left to read keep going*/
-		leido = read(fichero, guardado, BUFFER_SIZE); /*update the read once we rode the first 1024 Bytes*/
-	}
-	if (leido >= 0){
-		close(fichero);	/* If correctly read close the opened file*/
-	}
-	return 0;
-	}
+    
+    //Open the file, save its descriptor and check it to see if there's an error
+    descriptor = open(argv[1], O_RDONLY);
+    if (descriptor < 0) {
+        printf("The file doesn't exist. \n");    // If descriptor is equal to 0 there's no such file.
+        return(-1);
+    }
+    else{
+        nread = read(descriptor, buffer, BUFFER_SIZE);
+        if (nread != 0) {
+            do {
+                write(STDOUT_FILENO, buffer, nread);
+                nread = read(descriptor, buffer, BUFFER_SIZE);
+            }
+            while(nread != 0);
+        }
+    }
+    close(descriptor);  
+    return 0;
+}
 
+/*
+// The file cannot be opened.
+if ((infile = open(name, O_RDONLY)) < 0) {
+    return(-1);
+}
+*/
+
+/*
+The predefined descriptors are:
+    •	0 : Standard input (read from stdin).
+    •	1 : Standard output (write to stout).
+    •	2 : Error output (write to stderr).
+*/
